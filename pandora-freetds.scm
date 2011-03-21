74434f9599d8d5d9edf25d974675bf53cce86135
Peter Danenberg <pcd@roxygen.org>
2011-03-20 23:37:24 -0700

    width; imports

--- pandora-freetds.scm
+++ pandora-freetds.scm
@@ -1,7 +1,7 @@
 (module
  pandora-freetds
  *
- (import scheme chicken)
+ (import scheme chicken data-structures srfi-13)
 
  (use prometheus pandora freetds lru-cache uri-generic matchable)
 
@@ -40,24 +40,63 @@
 
  (define-method (*freetds-datastore* 'table self resend name)
    (let ((primary-keys
-          (self 'execute (list (format "SELECT [name] FROM syscolumns WHERE [id] IN (SELECT [id] FROM sysobjects WHERE [name] = '~a') AND colid IN (SELECT SIK.colid FROM sysindexkeys SIK JOIN sysobjects SO ON SIK.[id] = SO.[id] WHERE SIK.indid = 1 AND SO.[name] = '~a')" name name))))
+          (self 'execute
+                (list (format "SELECT [name] FROM syscolumns WHERE [id] IN
+  (SELECT [id] FROM sysobjects WHERE [name] = '~a') AND colid IN
+    (SELECT SIK.colid FROM sysindexkeys SIK
+       JOIN sysobjects SO ON SIK.[id] = SO.[id] WHERE
+       SIK.indid = 1 AND SO.[name] = '~a')" 
+                              name name))))
          (column-names
-          (map (match-lambda ((catalog schema name column-name ordinal default null? data-type max-length octet-length precision precision-radix scale datetime-precision character-set character-set-schema character-set-name collation-catalog collation-scheme collation-name domain-catalog domain-schema domain-name)
+          (map (match-lambda ((catalog
+                               schema
+                               name
+                               column-name
+                               ordinal
+                               default
+                               null?
+                               data-type
+                               max-length
+                               octet-length
+                               precision
+                               precision-radix
+                               scale
+                               datetime-precision
+                               character-set
+                               character-set-schema
+                               character-set-name
+                               collation-catalog
+                               collation-scheme
+                               collation-name
+                               domain-catalog
+                               domain-schema
+                               domain-name)
                               column-name))
                (self 'execute
-                     (list (format "SELECT * FROM information_schema.columns WHERE table_name = '~a'"
+                     (list (format "SELECT * FROM information_schema.columns WHERE
+                                      table_name = '~a'"
                                    name))))))
      (let ((table (resend #f 'table name)))
        (table 'set-primary-key-clauses! primary-keys)
        (for-each (lambda (column-name)
-                   (let ((name (string-translate (string-downcase column-name) "_" "-")))
+                   (let ((name (string-translate
+                                (string-downcase column-name) "_" "-")))
                      (let ((getter (string->symbol name))
-                           (setter (string->symbol (string-append "set-" name "!"))))
-                       (table 'add-column-slots! getter setter column-name))))
+                           (setter (string->symbol
+                                    (string-append "set-" name "!"))))
+                       (table 'add-column-slots!
+                              getter
+                              setter
+                              column-name))))
                  column-names) 
        table)))
 
- (define-method (*freetds-datastore* 'execute self resend sql #!optional (params '()))
+ (define-method (*freetds-datastore* 'execute
+                                     self
+                                     resend
+                                     sql
+                                     #!optional
+                                     (params '()))
    (let replace ((sql sql)
                  (replaced-sql '())
                  (params params))
@@ -81,7 +120,14 @@
                           replaced-sql)
                     (if parameter? (cdr params) params))))))
 
- (define-method (*freetds-datastore* 'fold self resend proc init sql #!optional (params '()))
+ (define-method (*freetds-datastore* 'fold
+                                     self
+                                     resend
+                                     proc
+                                     init
+                                     sql
+                                     #!optional
+                                     (params '()))
    (self 'execute sql params))
 
  (define-method (*freetds-datastore* 'name->column-clause self resend name)
